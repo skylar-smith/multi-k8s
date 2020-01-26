ea872999ae171cc0b2c01f5985948011d1115795
SavvySkylark <skylarsmithe@gmail.com>
2020-01-26 17:29:18 -0500

    correct set image typo

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ docker push savvyskylark/multi-client:$SHA
 docker push savvyskylark/multi-server:$SHA
 docker push savvyskylark/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=savvyskylark/multi-server:$SHA
-kubectl set image deployments/client-deployments server=savvyskylark/multi-client:$SHA
-kubectl set image deployments/worker-deployments server=savvyskylark/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=savvyskylark/multi-server:$SHA
+kubectl set image deployments/client-deployment server=savvyskylark/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=savvyskylark/multi-worker:$SHA
